12e99bd30a485f7b543d92d11c949947f3e53e28
DESKTOP-RDM1EP0\enisi <enisisov@hotmail.com>
2018-07-19 11:03:15 +0200

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket = io();
+      
+socket.on('connect', function()  {
+  console.log('Connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Enis',
+    text: 'Hey youu'
+  });
+});
+
+
+
+socket.on('disconnect', function ()  {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+    console.log('newMessage', message);
+});
