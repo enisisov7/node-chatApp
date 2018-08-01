7d26c4ecb99f2522eb91baecded6822adb81a449
DESKTOP-RDM1EP0\enisi <enisisov@hotmail.com>
2018-08-01 13:01:21 +0200

    Format timestemps using moment.js

---
+++ playground/time.js
@@ -0,0 +1,11 @@
+var moment = require('moment');
+
+//10:35
+//6:01
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+var createdAt = 1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'));
